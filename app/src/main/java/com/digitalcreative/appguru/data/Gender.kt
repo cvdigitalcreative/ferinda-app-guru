2aa74a0ea5c704eb067c109530f3c68caa67aad6
vandoc <ichvandiocta@gmail.com>
2020-08-18 09:46:12 +0700

    Add register murid layout

---
+++ app/src/main/java/com/digitalcreative/appguru/data/Gender.kt
@@ -0,0 +1,6 @@
+package com.digitalcreative.appguru.data
+
+data class Gender(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/digitalcreative/appguru/data/Religion.kt
@@ -0,0 +1,6 @@
+package com.digitalcreative.appguru.data
+
+data class Religion(
+    val id: Int,
+    val name: String
+)
