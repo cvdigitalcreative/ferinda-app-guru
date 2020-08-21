79d93e4643220718c8e1f4f63d2c86d00bc0a3b7
vandoc <ichvandiocta@gmail.com>
2020-08-21 22:40:59 +0700

    Add AddAssignmentActivity.kt layout

--- app/src/main/java/com/digitalcreative/appguru/ui/assignment/AssignmentActivity.kt
+++ app/src/main/java/com/digitalcreative/appguru/ui/assignment/AssignmentActivity.kt
@@ -3,13 +3,13 @@ package com.digitalcreative.appguru.ui.assignment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.digitalcreative.appguru.R
-import kotlinx.android.synthetic.main.activity_tugas.*
+import kotlinx.android.synthetic.main.activity_assignment.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class AssignmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tugas)
+        setContentView(R.layout.activity_assignment)
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
